5276cb5f510e1752a2c411c74e4098750f012305
Chris Missal <chris.missal@gmail.com>
2019-09-26 23:02:39 -0500

    WIP

--- csharp/build_tools.sh
+++ csharp/build_tools.sh
@@ -17,12 +17,12 @@ fi
 VERSION_NUMBER=$1
 # <directory name> <binary file name> pairs.
 declare -a FILE_NAMES=(          \
-  windows_x86 windows-x86_32.exe \
-  windows_x64 windows-x86_64.exe \
-  macosx_x86  osx-x86_32.exe     \
-  macosx_x64  osx-x86_64.exe     \
-  linux_x86   linux-x86_32.exe   \
-  linux_x64   linux-x86_64.exe   \
+  win32       win32.zip          \
+  win64       win64.zip          \
+  osx-32      osx-x86_32.zip     \
+  osx-64      osx-x86_64.zip     \
+  linux_32    linux-x86_32.zip   \
+  linux_64    linux-x86_64.zip   \
 )
 
 set -e
@@ -33,20 +33,21 @@ for((i=0;i<${#FILE_NAMES[@]};i+=2));do
   DIR_NAME=${FILE_NAMES[$i]}
   mkdir -p protoc/$DIR_NAME
 
-  if [ ${DIR_NAME:0:3} = "win" ]; then
-    TARGET_BINARY="protoc.exe"
-  else
-    TARGET_BINARY="protoc"
-  fi
-
   BINARY_NAME=${FILE_NAMES[$(($i+1))]}
-  BINARY_URL=http://repo1.maven.org/maven2/com/google/protobuf/protoc/${VERSION_NUMBER}/protoc-${VERSION_NUMBER}-${BINARY_NAME}
+  BINARY_URL=https://github.com/protocolbuffers/protobuf/releases/download/v${VERSION_NUMBER}/protoc-${VERSION_NUMBER}-${BINARY_NAME}
 
-  if ! wget ${BINARY_URL} -O protoc/$DIR_NAME/$TARGET_BINARY &> /dev/null; then
+  if ! wget ${BINARY_URL} -O protoc/$DIR_NAME/protoc.zip; unzip protoc/$DIR_NAME/protoc.zip; then
     echo "[ERROR] Failed to download ${BINARY_URL}" >&2
     echo "[ERROR] Skipped $protoc-${VERSION_NAME}-${DIR_NAME}" >&2
     continue
+
+    cd /protoc/$DIR_NAME unzip
+    cd ../../
   fi
 done
 
-nuget pack Google.Protobuf.Tools.nuspec
+
+
+
+
+#nuget pack Google.Protobuf.Tools.nuspec
